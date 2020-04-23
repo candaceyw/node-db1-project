a216f661afd5ca73d8b1fd5a8029812f1f39c0c1
Candace Wilson <candaceyw@gmail.com>
2020-04-22 20:10:20 -0400

    CRUD working and complete. Met MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
+const express = require('express');
+const accountsRouter = require('../accountsRouter');
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
+
+server.get('/', (req, res) => {
+	res.status(200).json('Success');
+}); //end server.get
 
 module.exports = server;
